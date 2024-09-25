8bcc1bfc141abb544cf0e98a0edc0c87965e310a
amir_rafigh <amir.rafigh.work@gmail.com>
2024-09-25 15:16:17 +0330

    finish the project

--- script/script.js
+++ script/script.js
@@ -1,57 +1,68 @@
 const img = document.getElementsByTagName('img');
 const dot = document.getElementsByClassName('dots');
 const numslide = document.querySelector('.num-slide');
-console.log(numslide)
+const cont_img =document.getElementsByClassName('container-img')[0];
+
 
 function back(){
-    for( var i = 4 ; i>=0 ; i--){
-
-        if(i == 0){
-            img[i].className ="";
-            dot[i].className="dots";
-            i=4;
-            img[i].className+="d-img";
-            dot[i].className+=" dot-color";
-            numslide.innerHTML = i + 1;
-
-            return
+    cont_img.className="container-img";
+    setTimeout(() => {
+        cont_img.className+=" animation"
+        for( var i = 4 ; i>=0 ; i--){
+    
+            if(i == 0){
+                img[i].className ="";
+                dot[i].className="dots";
+                i=4;
+                img[i].className+="d-img";
+                dot[i].className+=" dot-color";
+                numslide.innerHTML = i + 1;
+    
+                return
+            }
+            
+    
+            else if(img[i].className == "d-img"){
+                img[i].className ="";
+                dot[i].className="dots";
+                img[i-1].className+="d-img";
+                dot[i-1].className+=" dot-color";
+                numslide.innerHTML= i ;
+    
+                return
+            }
         }
         
-
-        else if(img[i].className == "d-img"){
-            img[i].className ="";
-            dot[i].className="dots";
-            img[i-1].className+="d-img";
-            dot[i-1].className+=" dot-color";
-            numslide.innerHTML= i ;
-
-            return
-        }
-    }
+    }, 100);
 }
 
 function next(){
-    for(var i = 0 ; i<img.length ; i++){
+    cont_img.className="container-img";
+    setTimeout(() => {
+        cont_img.className+=" animation"
         
-        if(i==4){
-            img[i].className="";
-            dot[i].className="dots";
-            i=0;
-            img[i].className+="d-img";
-            dot[i].className+=" dot-color";
-            numslide.innerHTML= i + 1
-
-            return
-        }
-       else if(img[i].className=="d-img"){
-            img[i].className="";
-            dot[i].className="dots";
-            img[i + 1].className+="d-img";
-            dot[i + 1].className+=" dot-color";
-            numslide.innerHTML = i + 2;
+        for(var i = 0 ; i<img.length ; i++){
             
-            return
+            if(i==4){
+                img[i].className="";
+                dot[i].className="dots";
+                i=0;
+                img[i].className+="d-img";
+                dot[i].className+=" dot-color";
+                numslide.innerHTML= i + 1
+    
+                return
+            }
+           else if(img[i].className=="d-img"){
+                img[i].className="";
+                dot[i].className="dots";
+                img[i + 1].className+="d-img";
+                dot[i + 1].className+=" dot-color";
+                numslide.innerHTML = i + 2;
+                
+                return
+            }
         }
-    }
+    }, 100);
 
 }
